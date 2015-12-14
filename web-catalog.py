d21bc1441a2ebd3465f3c2ec81058deaa5b7f6dc
Diogo Kersting <diogovk@gmail.com>
2015-12-14 17:02:39 -0200

    Add server validation of google sign in

--- web-catalog.py
+++ web-catalog.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python2
 
-from flask import Flask
+from flask import Flask, request
+from oauth2client import client, crypt
 
 app = Flask(__name__)
+MY_CLIENT_ID='969890289717-96158do2n0gntojond0bnrmor86gdriu.apps.googleusercontent.com'
 
 @app.route('/')
 def login_page():
     return app.send_static_file('google_sign_in.html')
 
+@app.route('/oauth_check', methods=['POST'])
+def oauth_check():
+    token = request.get_json()['token']
+    idinfo = client.verify_id_token(token, MY_CLIENT_ID)
+    if idinfo['aud'] != MY_CLIENT_ID:
+        return ('Wrong "client id"', 403)
+    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+        return ('Wrong issuer', 403)
+    return ('ok', 200)
+
+
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
 
