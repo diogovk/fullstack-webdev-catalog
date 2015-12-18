0c2ad2ff32ac362fbb3d3d208766a27bc7c9df4a
Diogo Kersting <diogovk@gmail.com>
2015-12-18 11:40:29 -0200

    separate javascript from home template

---
+++ static/page.js
@@ -0,0 +1,19 @@
+
+"use strict";
+console.log("starting");
+var request = window.superagent;
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    request.post('/oauth_check')
+        .send({ token: id_token })
+        .end(function(err, res){
+            if (res.text == "ok") {
+                alert("Logged in successfuly");
+            }
+        });
+}
