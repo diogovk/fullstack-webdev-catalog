cfdaaf7d3ebe82d117c9306fb8475afadc1f5024
Diogo Kersting <diogovk@gmail.com>
2015-12-18 14:36:02 -0200

    Add table Items

--- models.py
+++ models.py
@@ -2,6 +2,10 @@ from app import db
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), unique=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
 
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
 
