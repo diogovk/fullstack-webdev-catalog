e4e24e6637ebbaaeeeeba65f0211fd9b553ba2ca
Diogo Kersting <diogovk@gmail.com>
2015-12-18 15:01:13 -0200

    Add example items in seed_database

--- seed_database.py
+++ seed_database.py
@@ -1,9 +1,24 @@
 from app import db
-from models import Category
+from models import Category, Item
 
-for category_name in ["Soccer", "Basketball", "Skating", "Volleyball", "Handball", "Magic",
-        "Swimming", "Tennis"]:
+seed_data={
+        "Soccer": ["Ball", "Gloves", "Red Card"],
+        "Basketball": ["Ball", "Sneakers", "Hoop"],
+        "Skating": ["Ice Skates", "Roller Skates"],
+        "Volleyball": ["Ball", "Net"],
+        "Handball": ["Ball", "Goal"],
+        "Magic": ["Sleeves", "Playmat"],
+        "Swimming": ["Swimming Googles", "Swimsuit"],
+        "Tennis": ["Tennis Ball", "Racket"]
+        }
+
+for category_name, items in seed_data.items():
     category = Category(name = category_name)
     db.session.add(category)
+    db.session.commit()
+    for item_name in items:
+        item = Item(name = item_name, category_id = category.id)
+        db.session.add(item)
+        db.session.commit()
 
-db.session.commit()
+
