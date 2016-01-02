fc86f41904a5895c94ea2a4064dd872d0c56cda1
Diogo V. Kersting <diogovk@gmail.com>
2016-01-02 13:27:32 -0200

    Add new field to Items: image_file

--- models.py
+++ models.py
@@ -8,4 +8,5 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    image_file = db.Column(db.String(128))
 
