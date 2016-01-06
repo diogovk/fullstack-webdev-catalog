5e1e367de8b96f1efa0d7b7888aa2abe3aa32df0
Diogo V. Kersting <diogovk@gmail.com>
2016-01-06 12:44:57 -0200

    Add description field to Item model

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class Category(db.Model):
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(500))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
     image_file = db.Column(db.String(128))
 
