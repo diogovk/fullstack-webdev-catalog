141eeeecc818d8589ba9de92e602262e35c027d9
Diogo Kersting <diogovk@gmail.com>
2016-01-20 13:57:14 -0200

    PEP8 and small refactoring

--- web_catalog.py
+++ web_catalog.py
@@ -10,16 +10,12 @@ from models import Item
 from models import Category, Item
 from helpers import get_image_extension, save_image
 
-MY_CLIENT_ID='969890289717-96158do2n0gntojond0bnrmor86gdriu.apps.googleusercontent.com'
-
 
 @app.route('/')
 def home():
     categories = Category.query.all()
-    return render_template('home.html',
-            categories=categories,
-            flow=flow,
-            csrf_form = Form())
+    return render_template('home.html', categories=categories, flow=flow,
+                           csrf_form=Form())
 
 
 @app.route('/gconnect', methods=['POST'])
@@ -31,15 +27,15 @@ def gconnect():
 
 @app.route('/category/<int:id>/items')
 def list_items(id):
-    items = Item.query.filter_by(category_id = id).all()
-    return render_template('items.html', items=items, category_id = id);
+    items = Item.query.filter_by(category_id=id).all()
+    return render_template('items.html', items=items, category_id=id)
 
 
 @app.route('/category/<int:id>/items/new')
 def new_item(id):
     form = NewItemForm()
     action = "/category/%s/items" % id
-    return render_template('edit_item.html', form = form, action = action)
+    return render_template('edit_item.html', form=form, action=action)
 
 
 @app.route('/category/<int:id>/items', methods=['POST'])
@@ -48,27 +44,27 @@ def create_item(id):
     if form.validate_on_submit():
         file = request.files[form.image_file.name]
         saved_path = save_image(file)
-        new_item = Item(name = form.data["name"],
-                category_id = id,
-                description = form.data["description"],
-                image_file = saved_path)
+        new_item = Item(name=form.data["name"],
+                        category_id=id,
+                        description=form.data["description"],
+                        image_file=saved_path)
         db.session.add(new_item)
         db.session.commit()
         return "ok"
     action = "/category/%s/items" % id
-    return render_template('edit_item.html', form = form, action = action)
+    return render_template('edit_item.html', form=form, action=action)
 
 
 @app.route('/item/<int:id>', methods=['GET'])
 def show_item(id):
-    item = Item.query.filter_by(id = id).first()
-    csrf_form = Form()
-    return render_template('show_item.html', item = item, csrf_form = csrf_form)
+    item = Item.query.filter_by(id=id).first()
+    # CSRF needed for delete function
+    return render_template('show_item.html', item=item, csrf_form=Form())
 
 
 @app.route('/item/<int:id>', methods=['DELETE'])
 def delete_item(id):
-    item = Item.query.filter_by(id = id).first()
+    item = Item.query.filter_by(id=id).first()
     if item:
         db.session.delete(item)
         db.session.commit()
@@ -78,16 +74,16 @@ def delete_item(id):
 
 @app.route('/item/<int:id>/edit')
 def edit_item(id):
-    item = Item.query.filter_by(id = id).first()
+    item = Item.query.filter_by(id=id).first()
     if item:
         form = NewItemForm(obj=item)
-        return render_template('edit_item.html', form = form, action = item.url)
+        return render_template('edit_item.html', form=form, action=item.url)
     return "Not Found", 404
 
 
 @app.route('/item/<int:id>', methods=['PUT', 'POST'])
 def update_item(id):
-    item = Item.query.filter_by(id = id).first()
+    item = Item.query.filter_by(id=id).first()
     if not item:
         return "Not Found", 404
     form = NewItemForm(obj=item)
@@ -100,10 +96,9 @@ def update_item(id):
         item.description = form.data["description"]
         db.session.commit()
         return "ok"
-    return render_template('edit_item.html', form = form, action = item.url)
+    return render_template('edit_item.html', form=form, action=item.url)
 
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
-
