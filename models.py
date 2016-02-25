29163bc717e86509bd3682995df71c6c3218e23c
Diogo Kersting <diogovk@gmail.com>
2016-02-25 18:21:26 -0300

    Add User table, and user_id column to the item table

--- models.py
+++ models.py
@@ -15,6 +15,25 @@ class Category(db.Model):
                 "items": [item.serialize for item in self.items]
                 }
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+
+    @classmethod
+    def get_or_create(cls, email):
+        '''
+        Returns the user with a certain e-mail, creating one in the database
+        if it doesn't exist.
+        '''
+        user = db.session.query(cls).filter_by(email=email).first()
+        if user:
+            return user
+        else:
+            user = cls(email=email)
+            db.session.add(user)
+            db.session.commit()
+            return user
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,7 +41,11 @@ class Item(db.Model):
     description = db.Column(db.String(500))
     category_id = db.Column(
             db.Integer, db.ForeignKey('category.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     image_file = db.Column(db.String(128))
+
+    def is_owned_by(self, user):
+        self.user_id == user.id
 
     @property
     def url(self):
