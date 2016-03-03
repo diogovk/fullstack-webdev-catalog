e21f601ede98b355710adcece885c1fbabe4df58
Diogo V. Kersting <diogovk@gmail.com>
2016-03-03 08:37:11 -0300

    vagrant setup: install dependencies, remove redis

--- pg_config.sh
+++ pg_config.sh
@@ -1,25 +1,19 @@
 apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
-apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-flask python-sqlalchemy python-bs4
 apt-get -qqy install python-pip
-pip install bleach
-pip install oauth2client
-pip install requests
-pip install httplib2
-pip install redis
-pip install passlib
-pip install itsdangerous
-pip install flask-httpauth
+pip2 install oauth2client
+pip2 install requests
+pip2 install Flask-Migrate
+pip2 install dicttoxml
+pip2 install flask-wtf
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
-su vagrant -c 'createdb forum'
-su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
+su vagrant -c 'createdb webcatalog'
+cd /vagrant
+su vagrant -c 'python2 migrator.py db upgrade'
+su vagrant -c 'python2 seed_database.py'
 
-vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m\npython2 web_catalog.py to start the server"
 echo -e $vagrantTip > /etc/motd
 
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-make
-make install
